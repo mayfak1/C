3cf227a4545e5dc0e8e85faeec41a15c9559ea10
mayfak <konstantinmokrousov@MacBook-Air-Konstantin.local>
2024-10-15 23:15:56 +0700

    del

--- 2.1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    char name[20],surname[20];
-    printf("Введите свое имя и фамилию: \n");
-    scanf("%s %s",name,surname);
-    printf("%s %s",surname,name);
-    return 0;
-}
--- 2.3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    float a;
-    scanf("%f",&a);
-    printf("%.1f or %.1e\n",a,a);
-    printf("+%.3f or %.3E\n",a);
-    return 0;
-}
